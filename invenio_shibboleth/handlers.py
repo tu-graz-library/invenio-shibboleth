128067d5c82bac0f7be7c1e0eeb1bf6b014311e5
mb-wali <mb_wali@hotmail.com>
2021-04-20 14:09:49 +0200

    test: fixed pydocs

--- invenio_shibboleth/handlers.py
+++ invenio_shibboleth/handlers.py
@@ -67,14 +67,14 @@ def account_info(attributes, remote_app):
         # set the value to the session
         session["identifier"] = identifier
 
-    #TODO: get prefix
+    # TODO: get prefix
     prefix = "TUG"
 
     return dict(
         user=dict(
             email=email,
-            profile=dict(username=prefix+external_id.split('@')[0],
-            full_name=name + " " + surname),
+            profile=dict(username=prefix + external_id.split('@')[0],
+                         full_name=name + " " + surname),
         ),
         external_id=external_id,
         external_method=remote_app,
